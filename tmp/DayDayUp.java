361c2aeab09fdb8223a1e9dffde37937f233eebd
Pechola <pechola.lee@outlook.com>
2024-04-01 22:06:36 +0800

    pechola

--- tmp/DayDayUp.java
+++ tmp/DayDayUp.java
@@ -1,25 +1,81 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DayDayUp {
+    // main() for test
     public static void main(String [] args) {
-        System.out.println("hola");
-        List<List<Integer>> r = new ArrayList<>();
+        System.out.println("_hola_");
         DayDayUp t = new DayDayUp();
-        r = t.generate(5);
-        System.out.println(r);
+        
+        // TEST
+        int[] a = new int[] {7,1,5,3,6,4};
+        System.out.println(t.maxProfit(a));
     }
 
-    
-    /*
-     * 杨辉三角 3.28
-     * 被难住的是Java语法，垃圾python破坏手感
+
+    /* 4.1
+     * 最佳卖股票时机
+     * 首先我知道不要用两个for，那样不够优雅
+     * 直接分治，但没想到有O(n)的方法
      */
-    public List<List<Integer>> generate(int numRows) {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxRes = 0;
+        for (int i=0; i<prices.length; ++i) {
+            if (prices[i]<minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i]-minPrice > maxRes) {
+                maxRes = prices[i]-minPrice;
+            }
+        }
+        return maxRes;
+    }
+    // Divide & Conquer
+    public int maxProfit_DC(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        } else {
+            int start = 0;
+            int end = prices.length;
+            int mid = (start+end)/2;
+            int left = maxProfit(Arrays.copyOfRange(prices, start, mid));
+            int right = maxProfit(Arrays.copyOfRange(prices, mid, end));
+            int cross = maxProfitCross(prices, mid);
+            return Math.max(Math.max(left, right), cross);
+        }
+    }
+    public int maxProfitCross(int[] prices, int mid) {
+        if (prices.length <= 1) {
+            return 0;
+        } else {
+            int leftMin = prices[0];
+            int rightMax = prices[mid];
+            for (int i=0; i<mid; ++i) {
+                if (prices[i] < leftMin) {
+                    leftMin = prices[i];
+                }
+                if (prices[mid+i] > rightMax) {
+                    rightMax = prices[mid+i];
+                }
+            }
+            if (prices[prices.length-1] > rightMax) {
+                rightMax = prices[prices.length-1];
+            }
+            return rightMax - leftMin;
+        }
+    }
+
+
+    /* 3.28
+     * 杨辉三角 
+     * 被难住的是Java语法，垃圾Python破坏手感
+     */
+    public List<List<Integer>> generateYangHui(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-        for(int i=0;i<numRows;i++) {
+        for (int i=0; i<numRows; ++i) {
             List<Integer> tmp = new ArrayList<>();
-            for(int j=0;j<i+1;j++) {
+            for (int j=0; j<i+1; ++j) {
                 if (j==0 || j==i) {
                     tmp.add(1);
                 } else {
@@ -30,5 +86,19 @@ public class DayDayUp {
         }
         return res;
     }
-    
+    // 获取三角第i行(从1开始)，采用滚动数组，每行从右往左，而不用将前面结果全部保存
+    public List<Integer> getRow(int rowIndex) {
+        if (rowIndex==0) {
+            return null;
+        }
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        for (int i=1; i<=rowIndex; ++i) {
+            res.add(0);
+            for (int j=i; j>0; --j) {
+                res.set(j, res.get(j)+res.get(j-1));
+            }
+        }
+        return res;
+    }
 }
