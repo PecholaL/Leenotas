db230a410ea394719f38eabd0c3f61309a950504
Pechola <pechola.lee@outlook.com>
2024-04-22 21:23:02 +0800

    pechola

--- tmp/sql/learn.sql
+++ tmp/sql/learn.sql
@@ -1,3 +1,19 @@
+-- 4.19
+-- 重复的电子邮箱
+SELECT Email FROM
+{
+    SELECT Email, COUNT(Email) AS num
+    FROM Person
+    GRUOP BY Email
+} AS statistic
+WHERE num > 1
+;
+-- 自联结
+SELECT DISTINCT a.Email FROM Person as a, Person AS b
+WHERE a.Email = b.Email and a.Id != b.Id
+;
+
+
 -- 4.18
 -- 超过经理收入的员工
 SELECT 
