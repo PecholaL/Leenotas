29c9dbebbc307d6d7a2264801580970c21ae4bdf
PecholaL <pechola.lee@outlook.com>
2024-07-22 10:20:36 +0800

    pechola

--- 1.HOT100/ez.py
+++ 1.HOT100/ez.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
 
     # 两数之和
@@ -8,3 +11,17 @@ class Solution:
                 return [i, dict[target - nums[i]]]
             dict[nums[i]] = i
         return []
+
+    # 字母异位词分组
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        dict = {}
+        for str in strs:
+            k = "".join(sorted(list(str)))
+            if k not in dict:
+                dict[k] = [str]
+            else:
+                dict[k].append(str)
+        for key in dict:
+            res.append(dict[key])
+        return res
